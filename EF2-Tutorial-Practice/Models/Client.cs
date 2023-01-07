b8605a4324c52c4757372e189b33da8e36986d1d
BaghdadTheBrave <storchunv@ukr.net>
2023-01-07 16:05:16 +0200

    connected DB, created sort of buisness logick

---
+++ EF2-Tutorial-Practice/Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.Models
+{
+   public partial class Client
+    {
+        public string FirstLast
+        {
+            get => $"{NameClients} {SurnameClients}";
+        }
+    }
+}
--- EF2-Tutorial-Practice/Program.cs
+++ EF2-Tutorial-Practice/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Workshop.Data;
+using Workshop.Models;
+
+using workshopContext context = new workshopContext();
+foreach(Client c in context.Clients)
+{
+    Console.WriteLine($"Name: {c.FirstLast}");
+}
